70f97e8fe5cbb25db51df8e91f7c4c78a2a62a90
Matthew <mcaldwell@mediamarketers.com>
2015-10-06 14:41:03 -0400

    strip unnecessary nonsense and get to a true base

--- functions.php
+++ functions.php
@@ -1,66 +1,14 @@
 <?php
 add_action( 'after_setup_theme', 'name_your_theme_setup' );
-function name_your_theme_setup()
-{
-load_theme_textdomain( 'name_your_theme', get_template_directory() . '/languages' );
-add_theme_support( 'automatic-feed-links' );
-add_theme_support( 'post-thumbnails' );
-global $content_width;
-if ( ! isset( $content_width ) ) $content_width = 640;
-register_nav_menus(
-array( 'main-menu' => __( 'Main Menu', 'name_your_theme' ) )
-);
+function name_your_theme_setup() {
+  load_theme_textdomain( 'name_your_theme', get_template_directory() . '/languages' );
+  add_theme_support( 'post-thumbnails' );
+  register_nav_menus(
+    array( 'main_nav' => __( 'Main Menu', 'name_your_theme' ) )
+  );
 }
 add_action( 'wp_enqueue_scripts', 'name_your_theme_load_scripts' );
-function name_your_theme_load_scripts()
-{
-wp_enqueue_script( 'jquery' );
-}
-add_action( 'comment_form_before', 'name_your_theme_enqueue_comment_reply_script' );
-function name_your_theme_enqueue_comment_reply_script()
-{
-if ( get_option( 'thread_comments' ) ) { wp_enqueue_script( 'comment-reply' ); }
-}
-add_filter( 'the_title', 'name_your_theme_title' );
-function name_your_theme_title( $title ) {
-if ( $title == '' ) {
-return '&rarr;';
-} else {
-return $title;
-}
-}
-add_filter( 'wp_title', 'name_your_theme_filter_wp_title' );
-function name_your_theme_filter_wp_title( $title )
-{
-return $title . esc_attr( get_bloginfo( 'name' ) );
-}
-add_action( 'widgets_init', 'name_your_theme_widgets_init' );
-function name_your_theme_widgets_init()
-{
-register_sidebar( array (
-'name' => __( 'Sidebar Widget Area', 'name_your_theme' ),
-'id' => 'primary-widget-area',
-'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
-'after_widget' => "</li>",
-'before_title' => '<h3 class="widget-title">',
-'after_title' => '</h3>',
-) );
-}
-function name_your_theme_custom_pings( $comment )
-{
-$GLOBALS['comment'] = $comment;
-?>
-<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>"><?php echo comment_author_link(); ?></li>
-<?php 
-}
-add_filter( 'get_comments_number', 'name_your_theme_comments_number' );
-function name_your_theme_comments_number( $count )
-{
-if ( !is_admin() ) {
-global $id;
-$comments_by_type = &separate_comments( get_comments( 'status=approve&post_id=' . $id ) );
-return count( $comments_by_type['comment'] );
-} else {
-return $count;
-}
+function name_your_theme_load_scripts() {
+  wp_enqueue_script( 'jquery' );
+  wp_enqueue_style( 'style', get_template_directory_uri().'css/php/style.css/php' );
 }
--- single.php
+++ single.php
@@ -1,43 +1,4 @@
-<!-- PHP FILE FOR A SINGLE POST PAGE -->
-
-<!-- CALL IN HEADER.PHP -->
-
 <?php get_header(); ?>
 
-<section id="content" role="main">
-
-<!-- BEGIN IF/WHILE HAVE POSTS LOOP -->
-
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-<!-- CALL IN ENTRY.PHP -->
-
-	<?php get_template_part( 'entry' ); ?>
-
-<!-- CALL IN COMMENTS TEMPLATE -->
-
-	<?php if ( ! post_password_required() ) comments_template( '', true ); ?>
-
-<!-- END IF/WHILE HAVE POSTS LOOP -->
-
-	<?php endwhile; endif; ?>
-
-<!-- BEGIN POST-FOOTER -->
-
-	<footer class="footer">
-
-	<!-- CALL IN NAV-BELOW-SINGLE.PHP -->
-
-		<?php get_template_part( 'nav', 'below-single' ); ?>
-
-	</footer><!--/.footer-->
-
-</section><!--/#content-->
-
-<!-- CALL IN SIDEBAR.PHP -->
-
 <?php get_sidebar(); ?>
-
-<!-- CALL IN FOOTER.PHP -->
-
 <?php get_footer(); ?>
